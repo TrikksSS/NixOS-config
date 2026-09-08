4028952dd2dc042c822e8477c33761fa0288adea
TrikksSS <TrikksSS>
2026-09-07 21:21:58 -0400

    Add Flakes

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "A very basic flake";
+inputs = {
+
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+};
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
+
